fd9469b31a907c4153605d8c59ff65cd184e6b18
hasibur14 <hasib141415@gmail.com>
2024-04-27 18:07:33 +0600

    added database connection in mongodb

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');  //req package
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
 const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 //middleware
@@ -10,12 +13,46 @@ app.use(express.json());
 
 
 
+//assignment-10-artistry-canvas
+//KG4kqcwBYZ1VtdJc
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfxjcnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const coffeeCollection = client.db('artandcraft').collection('craft');
+
+
+
+
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
     res.send('assignment is running')
 })
-
 
 app.listen(port, () => {
     console.log(`running is running on port: ${port}`)
